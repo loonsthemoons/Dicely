d375c0e6e16d86ab396fc67388bff6e25eb73d9d
loonsthemoons <loonsthemoons@proton.me>
2025-07-26 18:03:53 +0200

    Initial commit

---
+++ src/client/java/dev/loons/dicely/DicelyClient.java
@@ -0,0 +1,10 @@
+package dev.loons.dicely;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DicelyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
